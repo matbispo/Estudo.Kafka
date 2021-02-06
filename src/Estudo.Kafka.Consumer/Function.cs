733e2c465a4c74ddbbdb020c22eda7d48aee3063
Mateus Bispo <math.l.bispo@gmail.com>
2021-02-06 14:09:24 -0300

    Adicionar arquivos de projeto.

---
+++ src/Estudo.Kafka.Consumer/Function.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Estudo.Kafka.Consumer
+{
+    class Function
+    {
+        static void Main()
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using(var c = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                c.Subscribe("test-topic");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed messagem '{cr.Value}' at: {cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException ce)
+                        {
+                            Console.WriteLine($"Error occured: {ce.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/Estudo.Kafka.Producer/Function.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Estudo.Kafka.Producer
+{
+    class Function
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig {BootstrapServers = "localhost:9092" };
+
+            using(var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    int count = 1;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("test-topic", 
+                            new Message<Null, string> {Value = $"test: {count}" });
+
+                        Console.WriteLine($"Delived '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+                        count++;
+                        Thread.Sleep(3000);
+                    }
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+                    Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+                }
+            }
+        }
+    }
+}
